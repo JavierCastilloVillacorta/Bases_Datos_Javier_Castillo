6900c0e4bbd509addc356910b523e49a42a3de15
javier <javier@dominio.com>
2018-12-30 12:12:45 -0500

    Proyecto Teriminado + base php

--- verPhp/agenda.sql
+++ verPhp/agenda.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 30-12-2018 a las 17:56:49
+-- Tiempo de generación: 30-12-2018 a las 18:12:02
 -- Versión del servidor: 10.1.36-MariaDB
 -- Versión de PHP: 7.2.11
 
@@ -38,6 +38,28 @@ CREATE TABLE `eventos` (
   `idUsuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(225) NOT NULL,
+  `email` varchar(225) NOT NULL,
+  `password` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `email`, `password`) VALUES
+(1, 'Juan', 'juan@gmail.com', '$2y$10$9Odn10eYrwrI7ltw6y958uLqHqkRzXlIqiSPbnC8QoK5P9Q5UlXgy'),
+(2, 'Carlos', 'carlos@gmail.com', '$2y$10$IuckQHgV6QhgtLZpokqIu.5.W9xO4roWRnQuRLE6rjhnChYS3mKiy'),
+(3, 'Pedro', 'pedro@gmail.com', '$2y$10$B3htKeGGj1DLv4RY6nrlq.75yiNqiy1QNytB0IfijN.GE5KG1Ek.e');
+
 --
 -- Índices para tablas volcadas
 --
@@ -50,6 +72,12 @@ ALTER TABLE `eventos`
   ADD KEY `idUsuario` (`idUsuario`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -58,6 +86,12 @@ ALTER TABLE `eventos`
 --
 ALTER TABLE `eventos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
